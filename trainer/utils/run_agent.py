8d3c484082059375403c1e26e66fa86462642087
Xinyue <dev.xinyue.zhao@gmail.com>
2026-09-10 11:50:24 +0200

    feat|refactor: new run_agent for antigravity , traces_writer to parser antigravity messages

--- trainer/utils/run_agent.py
+++ trainer/utils/run_agent.py
@@ -1,5 +1,9 @@
 from rich.pretty import pprint as pp
-from utils.stream_format import format_messages
+from utils.stream_format import format_messages, show_message
+from google.antigravity import Agent
+from google.antigravity.types import Text, Thought, ToolCall, ToolResult
+from google.antigravity.conversation.conversation import Conversation
+import sys
 
 _INVOKE_CONIFG = {"recursion_limit": 10000}
 
@@ -25,3 +29,44 @@ async def run_deepagents(agent, input_messages, stream_mode):
             config=_INVOKE_CONIFG,
         )
     return response
+
+
+async def run_antigravity(config, message, stream_mode) -> Conversation:
+    async with Agent(config) as agent:
+        if stream_mode:
+            response = await agent.chat(message)
+            async for chunk in response.chunks:
+                if isinstance(chunk, Text):
+                    show_message(chunk.text, "✨ Assistant", border_style="bold green")
+                elif isinstance(chunk, Thought):
+                    show_message(chunk.text, "🧠 Thought", border_style="bold blue")
+                elif isinstance(chunk, ToolCall):
+                    show_message(
+                        f"""Name: {chunk.name}
+Args:
+    {chunk.args}
+""",
+                        "Tool Call",
+                        border_style="bold cyan",
+                    )
+                elif isinstance(chunk, ToolResult):
+                    show_message(
+                        f"""Name: {chunk.name}
+Results:
+    {chunk.result}
+    
+Error:
+    {chunk.error}
+Exception:
+    {chunk.exception}
+""",
+                        "Tool Result",
+                        border_style="bold magenta",
+                    )
+                else:
+                    pass
+                sys.stdout.flush()
+            return agent.conversation
+        else:
+            response = await agent.chat(message)
+            return agent.conversation
--- trainer/utils/traces_writer.py
+++ trainer/utils/traces_writer.py
@@ -1,10 +1,11 @@
 import json
+import os
 from pathlib import Path
 
+from google.antigravity.types import Step
 from langchain_core.load import dumps
 from loguru import logger
 from pydantic import BaseModel, ConfigDict
-import os
 
 
 class TracesPath(BaseModel):
@@ -14,7 +15,7 @@ class TracesPath(BaseModel):
     abs_path: Path
 
 
-class TracesWriter(BaseModel):
+class _Cleaner(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
     def _remove_key(self, a_dict, key):
@@ -22,9 +23,17 @@ class TracesWriter(BaseModel):
             a_dict.pop(key)
         return a_dict
 
-    def _clean_kwargs(self, **kwargs):
-        cleaed_kwargs = {
-            "type": kwargs["type"],
+    def _remove_keys(self, a_dict, *keys):
+        for key in keys:
+            if key in a_dict:
+                a_dict.pop(key)
+        return a_dict
+
+
+class _LangChainMessageListCleaner(_Cleaner):
+    def __call__(self, **kwargs):
+        cleaned_kwargs = {
+            "source": kwargs["type"],
         }
         if kwargs["content"]:
             cleaned_content = (
@@ -32,18 +41,43 @@ class TracesWriter(BaseModel):
                 if isinstance(kwargs["content"], list)
                 else self._remove_key(kwargs["content"], "extras")
             )
-            cleaed_kwargs["content"] = cleaned_content
+            cleaned_kwargs["content"] = cleaned_content
 
         if "tool_calls" in kwargs and kwargs["tool_calls"]:
-            cleaed_kwargs["tool_calls"] = list(
+            cleaned_kwargs["tool_calls"] = list(
                 map(lambda x_dict: self._remove_key(x_dict, "id"), kwargs["tool_calls"])
             )
 
         if kwargs["type"] == "tool":
-            cleaed_kwargs["name"] = kwargs["name"]
-            cleaed_kwargs["status"] = kwargs["status"]
+            cleaned_kwargs["name"] = kwargs["name"]
+            cleaned_kwargs["status"] = kwargs["status"]
 
-        return cleaed_kwargs
+        return cleaned_kwargs
+
+
+class _AntigravityStepHistoryCleaner(_Cleaner):
+
+    def __call__(self, **kwargs):
+        cleaned_kwargs = {"soruce": kwargs["source"]}
+        if kwargs["content"]:
+            cleaned_kwargs["content"] = kwargs["content"]
+        if kwargs["thinking"]:
+            cleaned_kwargs["thinking"] = kwargs["thinking"]
+        if kwargs["tool_calls"]:
+            cleaned_kwargs["tool_calls"] = kwargs["tool_calls"]
+            cleaned_kwargs["tool_calls"] = list(
+                map(
+                    lambda x_dict: self._remove_keys(
+                        x_dict, "id", "step_id", "canonical_path", "server_name"
+                    ),
+                    kwargs["tool_calls"],
+                )
+            )
+        return cleaned_kwargs
+
+
+class TracesWriter(BaseModel):
+    model_config = ConfigDict(extra="forbid")
 
     def append(self, task_id, list_messages, root_path) -> TracesPath:
         dir_path = os.path.join(str(root_path), task_id)
@@ -51,15 +85,29 @@ class TracesWriter(BaseModel):
         Path(dir_path).mkdir(parents=True, exist_ok=True)
         with open(file_path, "a", encoding="utf-8") as f:
             json_str = dumps(list_messages, pretty=True)
-            invers_json = json.loads(json_str)
-            kwargs_list = list(
-                map(lambda x: self._clean_kwargs(**x["kwargs"]), invers_json)
-            )
-            kwargs_list_str = json.dumps(kwargs_list, indent=2, ensure_ascii=False)
-            f.write(kwargs_list_str)
 
+            # if Antigravity history steps
+            if isinstance(list_messages, list) and isinstance(list_messages[0], Step):
+                predumpy_list = list(
+                    map(
+                        lambda step: _AntigravityStepHistoryCleaner()(
+                            **step.model_dump(mode="json")
+                        ),
+                        list_messages,
+                    )
+                )
+            else:
+                invers_json = json.loads(json_str)
+                predumpy_list = list(
+                    map(
+                        lambda x: _LangChainMessageListCleaner()(**x["kwargs"]),
+                        invers_json,
+                    )
+                )
+
+            json_str = json.dumps(predumpy_list, indent=2, ensure_ascii=False)
+            f.write(json_str)
             abs_path = Path(file_path).resolve()
-
             traces_path = TracesPath(path=Path(file_path), abs_path=abs_path)
             logger.success(f"Write traces to {traces_path}")
 
